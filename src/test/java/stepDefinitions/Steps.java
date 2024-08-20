9ec261738ff2123da36e497e7302b7de2b7e05bd
sulakshana763 <sulakshana763@gmail.com>
2024-08-19 23:00:42 -0700

    first commit with base project framework

---
+++ src/test/java/stepDefinitions/Steps.java
@@ -0,0 +1,27 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class Steps {
+	
+	@Given("user navigates to the website javatpoint.com")
+	public void user_navigates_to_the_website_javatpoint_com() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Given("there user logs in through Login Window by using Username as {string} and Password as {string}")
+	public void there_user_logs_in_through_login_window_by_using_username_as_and_password_as(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("login must be successful.")
+	public void login_must_be_successful() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+}
+
+
